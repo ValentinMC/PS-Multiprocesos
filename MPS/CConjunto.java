2fd47d8c23bf6927ac580c5e5d73a319104a765e
ValentinMC <valentincalin.99@gmail.com>
2020-05-11 20:48:29 +0200

    Ejercicio 3 corregido

--- MPS/CConjunto.java
+++ MPS/CConjunto.java
@@ -1,8 +1,45 @@
 package MPS;
+import java.util.ArrayList;
 
-public class CConjunto{
+public class CConjunto {
     public static void main(String[] args) {
-        //ArrayList alN = args[0];
-        //System.out.println(alN.get(0).toString());
+        ArrayList<Integer> iValor = new ArrayList<>();
+        //Aniadimos los argumentos a un arrayList de integer directamente
+        for(int i = 0; i<args.length;i++)
+            iValor.add(Integer.parseInt(args[i]));
+
+        //Hacemos el calculo de la moda
+        System.out.println("La moda es: "+mdCalcularModa(iValor));
+        //Hacemos el calculo de la media
+        System.out.println("La media es: "+mdCalcularMedia(iValor));
+        
     }//main()
+    static double mdCalcularMedia(ArrayList<Integer> ialNumeros){
+        double dResultado = 0.0;
+        int iSumaNumeros = 0;
+
+        for (int i:ialNumeros)
+            iSumaNumeros +=i;
+
+        dResultado = (double)iSumaNumeros/ialNumeros.size();
+        return dResultado;
+    }//mdCalcularMedia()
+
+    static double mdCalcularModa(ArrayList<Integer> ialNumeros){
+        int iMaxRep= 0;
+        double dModa= 0;
+
+        for(int i:ialNumeros){
+            int iRep= 0;
+            for(int j:ialNumeros){
+                if(i==j)
+                    iRep++;
+                if(iRep>iMaxRep){
+                    dModa = i;
+                    iMaxRep = iRep;
+                }//if
+            }//for j
+        }//for i
+        return dModa;
+    }//mdCalcularModa()
 }//CConjunto
--- MPS/Ej3.java
+++ MPS/Ej3.java
@@ -5,52 +5,67 @@
  ejercicio, uno donde se imprima por pantalla el resultado y otro en un fichero se 
  guarden todos los resultados.  
 */
-/*
-        ------------------COMO HACERLO-------------
-  - Guardamos los numeros en un Array de String (?) y luego los convertimos a int
-  - Iniciamos un ProcessBuilder y le pasamos el array (?)
-  - V.1 Imprimimos por pantalla // V.2 Guardamos en un fichero
-  
-*/
 package MPS;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Ej3{
     static Scanner oTeclado = new Scanner(System.in);
 
     public static void main(String[] args) {
-        List<String> numeros;
+
+        //Creamos un ArrayList para introducir los comandos y valores
+        ArrayList<String> alComandos = new ArrayList<>();
+        //Primero añadimos los parametros iniciales
+        alComandos.add("java");
+        alComandos.add("CConjunto");
+
         boolean bSeguir = true;
-        //Creamos un Array para introducir los valores y un contador para las posiciones del Array.
         //Pedimos los valores
         while(bSeguir){
             System.out.println("Dime el un numero o introduzca FIN para completar la operacion.");
             //Metemos el valor en un avariable local
             String sNumero = oTeclado.nextLine();
             //Verificamos el valor que nos ha introducido el usuario
-            if(sNumero.equals("FIN")){
+            if(sNumero.equalsIgnoreCase("FIN")){
                 bSeguir = false;
-                lanzarOperacion(numeros);
-            }
-            else
-                numeros.add(sNumero); 
+                lanzarOperacionV1(alComandos);
+                lanzarOperacionV2(alComandos);
+            }else
+                alComandos.add(sNumero); 
             //if-else
         }//while
-        lanzarOperacion(numeros);
+        
     }//main()
     
-    static void lanzarOperacion(List alNumero){
-        String sClase = "CConjunto";
+    static void lanzarOperacionV1(ArrayList<String> alNumeros){
+        //Aqui solo nos hara falta el arrayList
         String linea;
         try{
-            Process p = new ProcessBuilder("java",sClase,alNumero).start();
+            Process p = new ProcessBuilder(alNumeros).start();
+
+            //Imprimimos el resultado por pantalla
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while((linea = br.readLine())!=null)
+            while((linea=br.readLine())!=null)         
                 System.out.println(linea);
+
         }catch(Exception e){
             System.out.println(e);
         }//try-catch
-    }//lanzarOperacion()
+    }//lanzarOperacionV1()
+
+    static void lanzarOperacionV2(ArrayList<String> alNumeros){
+        File Fichero = new File("ResultadoEj3.txt");
+        try{
+            //Imprimimos el resultado en un fichero
+            ProcessBuilder pb = new ProcessBuilder(alNumeros);
+            pb.redirectErrorStream(true);
+            pb.redirectOutput(Fichero);
+            pb.start();
+        }catch(Exception e){
+            System.out.println(e);
+        }//try-catch
+    }//lanzarOperacionV2
 }//Ej3
