2195714478c260defc3922ecf8a4264848aa28f0
ValentinMC <valentincalin.99@gmail.com>
2020-05-12 23:39:04 +0200

    pequeñas mejoras ConVocales

--- MPS/ContVocales.java
+++ MPS/ContVocales.java
@@ -2,16 +2,15 @@ package MPS;
 import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ContVocales {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
         //Leemos el fichero y la vocal
         File f = new File(args[0]);
         String sVocal = args[1];
-        String sFrase="";
+        StringBuilder sFrase= new StringBuilder();
 
         try{
             FileReader fr = new FileReader(f);
@@ -20,7 +19,7 @@ public class ContVocales {
                 //leemos las frases del fichero
                 String sAux = br.readLine();
                 while(sAux!=null){
-                    sFrase += sAux;
+                    sFrase.append(sAux);
                     sAux = br.readLine();
                 }//while()
             }catch(EOFException eof){
@@ -29,11 +28,11 @@ public class ContVocales {
             br.close();
             fr.close();
         }catch(IOException io){
-            System.out.println(io);
+            System.out.println(io.getMessage());
         }//try-catch
 
         //Contamos las vocales y las sacamos por pantalla para que se guarde el valor en el fichero
-        System.out.println(""+mvContador(sFrase, sVocal));
+        System.out.println(""+mvContador(sFrase.toString(), sVocal));
     }//main()
 
     public static int mvContador(String lineas,String sVocal){
@@ -43,10 +42,10 @@ public class ContVocales {
             char [] c = lineas.toCharArray();
             char [] VocalMinuscula = sVocal.toLowerCase().toCharArray();
             char [] VocalMayuscula = sVocal.toUpperCase().toCharArray();
-            for(int i=0;i<c.length;i++)
-                if (c[i] == VocalMinuscula[0] || c[i] == VocalMayuscula[0])
+            for (char letra : c)
+                if (letra == VocalMinuscula[0] || letra == VocalMayuscula[0])
                     iContador++;
-            
+
             return iContador;
         }else
             return 0;
