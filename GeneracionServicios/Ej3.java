8043254cc72d50c7b4d853dbce181e3c3d6f2c6d
ValentinMC <valentincalin.99@gmail.com>
2020-05-13 12:29:42 +0200

    Identificacion de IP conseguida. Falta identificar el enlace

--- GeneracionServicios/Ej3.java
+++ GeneracionServicios/Ej3.java
@@ -1,13 +1,35 @@
+package GeneracionServicios;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 /*
  Crear un programa Java que identifique si la dirección IP 
  es unicast, multicast y el tipo de alcance de la dirección.
 */
-package GeneracionServicios;
-public class Ej3{
-    public static void main(String[] args) {
-        /*
-            Utilizar este link
-            https://www.baeldung.com/java-broadcast-multicast
-        */
+/*
+    Utilizar este link
+    https://www.baeldung.com/java-broadcast-multicast
+*/
+public class Ej3 {
+    public static void main(String[] args) throws UnknownHostException, SocketException {
+        Scanner oTeclado = new Scanner(System.in);
+        
+        //Pedimos una IP
+        System.out.println("Digame la IP que quiera identificar");
+        String sIP = oTeclado.nextLine();
+
+        //Miramos a ver si es multicast o unicast
+        InetAddress IP = InetAddress.getByName(sIP);
+        if(IP.isMulticastAddress())
+            System.out.println("Tu IP es multicast!");
+        else if(IP.isLoopbackAddress())
+            System.out.println("Tu IP es unicast!");
+        else
+            System.out.println("No hemos conseguido identificar tu IP");
+
+        //Comprobamos el tipo de enlace de la direccion
+
+        oTeclado.close();
     }//main()
 }//Ej3
