b4bc447ac12a80b5f010a6df3951130dde6c3d64
ValentinMC <valentincalin.99@gmail.com>
2020-05-22 09:05:50 +0200

    Creacion de ejercicios restantes SEGURIDAD

---
+++ Seguridad/Ej3.java
@@ -0,0 +1,10 @@
+package Seguridad;
+/*
+Crear un programa en Java que realice una firma digital de un fichero dado por
+parámetro
+*/
+public class Ej3{
+  public static void main(String[] args) {
+
+  }//main()
+}//Ej3
---
+++ Seguridad/Ej4.java
@@ -0,0 +1,11 @@
+package Seguridad;
+/*
+Crear un programa en Java que realice un cifrado simétrico, de manera que dado un archivo de texto,
+cree otro fichero con la información cifrada. Por otro lado, se podrá descifrar el fichero siempre que
+primero se haya cifrado
+*/
+public class Ej4{
+  public static void main(String[] args) {
+
+  }//main()
+}//Ej4
