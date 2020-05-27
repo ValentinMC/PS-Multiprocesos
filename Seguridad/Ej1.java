627df90fa96462bce723b5363cbb411869d2818d
ValentinMC <valentincalin.99@gmail.com>
2020-05-27 19:49:10 +0200

    Ejercicio 3 Seguridad inicializado pero no terminado

--- Seguridad/Ej1.java
+++ Seguridad/Ej1.java
@@ -4,33 +4,32 @@ public class Ej1 {
     //Utilizare el metodo CESAR para este ejercicio
 
 	static Scanner oTeclado = new Scanner(System.in);
-	public static void main (String ars[]){ 
+	public static void main (String ars[]){
         //Valores para el mensaje, mensaje encriptado, mensaje desencriptado y valor de desplazamiento
         String sMensajeC = "";
         String sMensajeE = "";
         String sMensajeD = "";
         int iDesplazamiento = 0;
-        
-        
+
+
         //Pedimos el mensaje y el desplazamiento al usuario
 		System.out.println("Dime una frase para cifrar");
         sMensajeC = oTeclado.nextLine();
 		System.out.println("Dime un desplazamiento");
         iDesplazamiento = oTeclado.nextInt();
-            
+
 		//Char Array para el mensaje cifrado
         char caCifrado[] =  sMensajeC.toCharArray();
 
         //Char Array del mensaje descifrado
         char caDescifrado[];
-        
+
 		try{
             //Ciframos el mensaje
 			for(int i = 0; i < caCifrado.length; i++){
 				caCifrado[i] = (char)(caCifrado[i]+iDesplazamiento);
-            }//for 
-
-            //Lo mostramos
+      }//for
+      //Lo mostramos
 			sMensajeE = String.valueOf(caCifrado);
 			System.out.println("El mensajo encriptado es: " +sMensajeE);
 
--- Seguridad/Ej3.java
+++ Seguridad/Ej3.java
@@ -1,10 +1,76 @@
-package Seguridad;
 /*
-Crear un programa en Java que realice una firma digital de un fichero dado por
-parámetro
+Crear un programa en Java que realice una firma digital de un fichero dado por parámetro
 */
-public class Ej3{
-  public static void main(String[] args) {
 
-  }//main()
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.util.Scanner;
+
+//import sun.misc.BASE64Encoder;
+
+public class Ej3 {
+    static Scanner oTeclado = new Scanner(System.in);
+    static String sFichero;
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException {
+
+        if (args.length != 1) {
+            System.err.println("Uso: java FirmaDigital \"texto a firmar\"");
+            System.exit(1);
+        }
+
+        System.out.println("Generando un par RSA...");
+        KeyPairGenerator generador = KeyPairGenerator.getInstance("RSA");
+        generador.initialize(1024);
+        KeyPair parClaves = generador.genKeyPair();
+        System.out.println("Generando el par de claves.");
+
+        // Tomar los bytes de datos a firmar del primer argumento
+        byte[] datos = args[0].getBytes("UTF8");
+
+        // Obtener instancia del objeto Signature e inicializarlo con
+        // la clave privada para firmarlo
+        Signature firma = Signature.getInstance("MD5WithRSA");
+        firma.initSign(parClaves.getPrivate());
+
+        // Prepara la firma de los datos
+        firma.update(datos);
+
+        // Firmar los datos
+        byte[] bytesFirma = firma.sign();
+
+        // Mostrar en ASCII
+        /*System.out.println("\nFirma:\n" +
+            new BASE64Encoder().encode(bytesFirma));
+        */
+        // Ahora procedemos a verificar la firma. Para ello necesitaremos
+        // reinicializar el objeto Signature con la clave pública.
+        // Esto hace un reset de los datos de la firma con lo que hay que
+        // pasárselos de nuevo para hacer el update.
+        firma.initVerify(parClaves.getPublic());
+
+        // Pasar los datos que fueron firmados
+        firma.update(datos);
+    
+        // Verificar
+        boolean verificado = false;
+        try {
+        verificado = firma.verify(bytesFirma);
+        } catch (SignatureException se) {
+        verificado = false;
+        }
+
+        if (verificado) {
+        System.out.println("\nFirma verificada.");
+        } else {
+        System.out.println("\nFirma incorrecta.");
+        }
+
+
+    }//main()
 }//Ej3
